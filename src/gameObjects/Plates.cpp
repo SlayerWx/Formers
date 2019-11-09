f32343660c1a7987673ed8d470bf707e5010bf7a
Maxi Ruffo <maxi_cs_95@hotmail.com>
2019-11-09 01:22:28 -0300

    creada funcion de dibujado y variables para el posicionamiento y dibujado de la misma

--- src/gameObjects/Plates.cpp
+++ src/gameObjects/Plates.cpp
@@ -7,3 +7,36 @@ Plates::Plates()
 Plates::~Plates()
 {
 }
+void Plates::drawMe()
+{
+	switch (myForm)
+	{
+	case circle:
+		DrawCircle(myPosition.x, myPosition.y, radius, myColor);
+		break;
+	case rectangle:
+		DrawRectangle(myPosition.x, myPosition.y, radius, radius, myColor);
+		break;
+	case triangle:
+		DrawTriangle({ myPosition.x,myPosition.y - radius/4 }, 
+					 { myPosition.x - radius / 2,myPosition.y + radius / 4 },
+					 { myPosition.x + radius / 2,myPosition.y + radius / 4 }, myColor);
+		break;
+	}
+}
+Vector2 Plates::getPosition()
+{
+	return myPosition;
+}
+void Plates::setPosition(Vector2 newPos)
+{
+	myPosition = newPos;
+}
+void Plates::setAlredyChange(bool newRedyChange)
+{
+	alredyChange = newRedyChange;
+}
+bool Plates::getAlredyChange()
+{
+	return alredyChange;
+}
--- src/gameObjects/Plates.h
+++ src/gameObjects/Plates.h
@@ -1,12 +1,25 @@
 #ifndef PLATES_H
 #define PLATES_H
+#include "raylib.h"
+enum Form
+{
+	circle, rectangle, triangle
+};
 class Plates
 {
 public:
 	Plates();
 	~Plates();
-
+	void drawMe();
+	Vector2 getPosition();
+	void setPosition(Vector2 newPos);
+	void setAlredyChange(bool newRedyChange);
+	bool getAlredyChange();
 private:
-
+	Vector2 myPosition;
+	float radius;
+	Form myForm;
+	Color myColor;
+	bool alredyChange;
 };
 #endif
