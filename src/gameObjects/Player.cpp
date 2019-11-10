d4f3161aeeba56e28cd3b641fc1d675429d78523
Maxi Ruffo <maxi_cs_95@hotmail.com>
2019-11-09 23:26:22 -0300

    creando movimiento de personaje con lerpeo

--- src/gameObjects/Player.cpp
+++ src/gameObjects/Player.cpp
@@ -1,8 +1,20 @@
 #include "Player.h"
-
+#include <iostream>
 Player::Player()
 {
-
+	speed = 4.0f;
+	distance = 30.0f;
+	myBody.x = 0;
+	myBody.y = 0;
+	myColor = RED;
+	myBody.width = 30.0f;
+	myBody.height = 30.0f;
+	stopAction = false;
+	executingMovement = 0.0f;
+	up = false;
+	down = false;
+	right = false;
+	left = false;
 }
 
 Player::~Player()
@@ -14,4 +26,83 @@ void Player::drawMe()
 #if DEBUG
 	DrawRectangleRec(myBody, myColor);
 #endif
+}
+void Player::input()
+{
+	if (!up && !down && !left && !right)
+	{
+		if (IsKeyDown(KEY_W))
+		{
+			up = true;
+			startPosition.x = myBody.x;
+			startPosition.y = myBody.y;
+			endPosition.x = startPosition.x;
+			endPosition.y = startPosition.y - distance;
+		}
+		if (IsKeyDown(KEY_S))
+		{
+			down = true;
+			startPosition.x = myBody.x;
+			startPosition.y = myBody.y;
+			endPosition.x = startPosition.x;
+			endPosition.y = startPosition.y + distance;
+		}
+		if (IsKeyDown(KEY_A))
+		{
+			left = true;
+			startPosition.x = myBody.x;
+			startPosition.y = myBody.y;
+			endPosition.x = startPosition.x - distance;
+			endPosition.y = startPosition.y;
+		}
+		if (IsKeyDown(KEY_D))
+		{
+			right = true;
+			startPosition.x = myBody.x;
+			startPosition.y = myBody.y;
+			endPosition.x = startPosition.x + distance;
+			endPosition.y = startPosition.y;
+		}
+	}
+}
+void Player::move()
+{
+	if (stopAction)
+	{
+		executingMovement = 0.0f;
+		up = false;
+		down = false;
+		left = false;
+		right = false;
+		stopAction = false;
+	}
+	if (executingMovement > 1.0f)
+	{
+		executingMovement = 1.0f;
+		stopAction = true;
+	}
+	if (up)
+	{
+		myBody.y = lerp(startPosition.y, endPosition.y, executingMovement);
+		executingMovement += speed * GetFrameTime();
+	}
+	if (down) 
+	{ 
+		myBody.y = lerp(startPosition.y, endPosition.y, executingMovement);
+		executingMovement += speed * GetFrameTime();
+	}
+	if (left) 
+	{ 
+		myBody.x = lerp(startPosition.x, endPosition.x, executingMovement);
+		executingMovement += speed * GetFrameTime();
+	}
+	if (right)
+	{
+		myBody.x = lerp(startPosition.x, endPosition.x, executingMovement);
+		executingMovement += speed * GetFrameTime();
+	}
+}
+float Player::lerp(float start, float end, float percent)
+{
+	return (start + percent * (end - start));
 }
--- src/gameObjects/Player.h
+++ src/gameObjects/Player.h
@@ -7,8 +7,21 @@ public:
 	Player();
 	~Player();
 	void drawMe();
+	void input();
+	void move();
 private:
 	Rectangle myBody;
 	Color myColor;
+	float speed;
+	float executingMovement;
+	float distance;
+	bool down;
+	bool up;
+	bool left;
+	bool right;
+	bool stopAction;
+	Vector2 startPosition;
+	Vector2 endPosition;
+	float lerp(float start, float end, float percent);
 };
 #endif
