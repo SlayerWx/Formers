5be5ecec68e0c531d4388cb3d2635c363f522cf4
Maxi Ruffo <maxi_cs_95@hotmail.com>
2019-11-09 23:27:02 -0300

    creada ventana de juego y loop general

--- src/game/Formers.cpp
+++ src/game/Formers.cpp
@@ -1,9 +1,55 @@
 #include "Formers.h"
-
 Formers::Formers()
 {
+	InitWindow(screenWidth, screenHeight, "raylib [core] example - mouse input");
+	gamestatus = GAME;
+	game = new Game();
+	inGame = true;
 }
 
 Formers::~Formers()
 {
+	if (game) delete game;
+}
+void Formers::Play()
+{
+	while (inGame)
+	{
+		Init();
+		Update();
+		Draw();
+	}
+}
+void Formers::Init()
+{
+
+}
+void Formers::Update()
+{
+	switch (gamestatus)
+	{
+	case MENU:
+		break;
+	case GAME:
+		game->Update();
+		break;
+	case CREDITS:
+		break;
+	}
+}
+void Formers::Draw()
+{
+	BeginDrawing();
+	ClearBackground(BLACK);
+	switch (gamestatus)
+	{
+	case MENU:
+		break;
+	case GAME:
+		game->Draw();
+		break;
+	case CREDITS:
+		break;
+	}
+	EndDrawing();
 }
--- src/game/Formers.h
+++ src/game/Formers.h
@@ -1,12 +1,25 @@
 #ifndef FORMERS_H
 #define FORMERS_H
+#include "raylib.h"
+#include "gameElements/Game.h"
+enum GameStage
+{
+	MENU,GAME,CREDITS
+};
 class Formers
 {
 public:
 	Formers();
 	~Formers();
-
+	void Play();
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+	GameStage gamestatus;
+	Game* game;
 private:
-
+	void Init();
+	void Update();
+	void Draw();
+	bool inGame;
 };
 #endif
