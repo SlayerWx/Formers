3413887a5f7ac24095cde3d18181b7dcab09501a
Maxi Ruffo <maxi_cs_95@hotmail.com>
2019-11-13 09:24:40 -0300

    corrigiendo nombre en game.h

--- src/gameElements/Game.h
+++ src/gameElements/Game.h
@@ -1,7 +1,7 @@
 #ifndef GAME_H
 #define GAME_H
 #include "gameObjects/Player.h"
-#include "gameElements/levels/Level1.h"
+#include "gameElements/levels/Level.h"
 #include "gameElements/levels/zones/Zone.h"
 class Game
 {
